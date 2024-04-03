6f0d2f0f4c39f236e492e16ab1902faa8b674a95
Sir Ronney Otieno <ronvi035@gmail.com>
2024-04-03 14:01:19 +0300

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-export default function App() {
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>App</div>
   )
 }
+
+export default App
